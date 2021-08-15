0050816d6ee6c8eca533e563837f82da446c8368
jash.liao <jash.liao@gmail.com>
2021-08-15 20:46:59 +0800

    Add 20210815163

---
+++ test code/build-clocks-Desktop_Qt_5_12_11_MinGW_32_bit-Release/release/123.bat
@@ -0,0 +1,8 @@
+CB_Win_ldd.exe clocks.exe
+CB_Win_ldd.exe libgcc_s_dw2-1.dll
+CB_Win_ldd.exe libwinpthread-1.dll
+CB_Win_ldd.exe Qt5Cored.dll
+CB_Win_ldd.exe Qt5Guid.dll
+CB_Win_ldd.exe Qt5Qmld.dll
+CB_Win_ldd.exe Qt5Quickd.dll
+pause
